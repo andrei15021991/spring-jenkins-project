9f1176d25a9df4b9ed7cd70f98e20d105df4b583
Andrei <mrsmurfn1@gmail.com>
2022-07-06 12:58:28 +0300

    first commit

---
+++ src/main/java/by/vazovikov/jenkins/SpringJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package by.vazovikov.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/vazovikov/jenkins/SpringJenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vazovikov.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
